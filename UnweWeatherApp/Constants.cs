1e9ed31149bb2d70873b5eb14a5df8306f54fb24
Vasilena <vt.merica@gmail.com>
2022-05-04 23:27:07 +0300

    Add Project

---
+++ UnweWeatherApp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace UnweWeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/forecast";
+        public static string OpenWeatherMapAPIKey = "aea1420e7b4422af45b977da31b2a192";
+    }
+}
---
+++ UnweWeatherApp/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace UnweWeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        WeatherService _WeatherService;
+        public MainPage()
+        {
+            InitializeComponent();
+            GetWeatherWithGeoLoaction();
+            _WeatherService = new WeatherService(); 
+        }
+
+
+        string GenerateRequestUriGeo(string endpoint, double lati, double longt)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?lat={lati}";
+            requestUri += $"&lon={longt}";
+            requestUri += "&units=imperial";
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+            requestUri += "&units=metric"; // or units = metric
+            requestUri += "&cnt=2";
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+
+        private WeatherService Get_WeatherService()
+        {
+            return _WeatherService;
+        }
+
+        private async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _WeatherService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+
+        }
+        private async void GetWeatherWithGeoLoaction ()
+        {
+            var location = await Geolocation.GetLocationAsync();
+            if (location !=  null)
+            {
+
+
+                WeatherData weatherData = await _WeatherService.GetWeatherData(GenerateRequestUriGeo(Constants.OpenWeatherMapEndpoint, location.Latitude, location.Longitude)); 
+                BindingContext = weatherData;
+            }
+            
+        }
+         
+
+    }
+}
+
